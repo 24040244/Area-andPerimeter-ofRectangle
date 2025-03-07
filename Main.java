ee3277fb39b75fa7189a2cbfa93da656a3120a63
24040244 <thembelihle.sibanda152@gmail.com>
2025-03-07 20:25:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        double height=4.6;
+        double base=7,43;
+        double area = height*base;
+       double perimeter=2*(base+ height);
+
+        System.out.println("the area of rectangle is"+area);
+        System.out.println("the perimeter of rectangle is"+perimeter);
+    }
+}
